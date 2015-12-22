3d94764019da64befecdc23ae9ea2e3f167156cc
David Cox <losttime.shuffle@gmail.com>
2015-12-22 16:20:12 -0700

    Can copy button content to the clipboard

--- app/views/main.js
+++ app/views/main.js
@@ -1,3 +1,13 @@
+var clipboard = require("nativescript-clipboard");
+
 exports.loaded = function() {
 	console.log('page loaded');
 }
+
+exports.buttonTapped = function(eventData) {
+	console.log('button tapped', eventData.eventName, eventData.object, eventData.object.text);
+	// put the emoji in the clipboard
+	clipboard.setText(eventData.object.text).then(function() {
+		console.log('OK, ' + eventData.object.text + ' copied to the clipboard');
+	});
+};
