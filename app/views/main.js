c87bb58a86d26fd3ba852db6bf4063c14eeee937
David Cox <losttime.shuffle@gmail.com>
2016-01-16 15:11:23 -0700

    Make emojis searchable

--- app/views/main.js
+++ app/views/main.js
@@ -1,6 +1,7 @@
-var clipboard = require("nativescript-clipboard");
-var observableModule = require("data/observable");
-var observableArrayModule = require("data/observable-array");
+var clipboard = require('nativescript-clipboard');
+var observableModule = require('data/observable');
+var observableArrayModule = require('data/observable-array');
+var view = require('ui/core/view');
 
 var emojiSet = require('emojione/emoji_strategy.json');
 
@@ -8,12 +9,28 @@ var emojiArray = [];
 for (var key in emojiSet) {
 	emojiArray.push({
 		value: String.fromCodePoint(parseInt(emojiSet[key].unicode, 16)),
+		keywords: emojiSet[key].keywords.split(' ')
 	});
 }
 
+var displayableEmojiList = new observableArrayModule.ObservableArray(emojiArray);
+
 var pageData = new observableModule.Observable({
-	displayableEmojiList: new observableArrayModule.ObservableArray(emojiArray)
+	emojiList: displayableEmojiList,
+	query: ''
 });
+
+function copy(text) {
+	clipboard.setText(text).then(function() {
+		console.log('OK, ' + text + ' copied to the clipboard');
+	});
+}
+
+function matches(emoji, text) {
+	return emoji.keywords.some(function(keyword) {
+		return keyword.includes(text);
+	});
+}
 
 exports.loaded = function(args) {
 	console.log('page loaded');
@@ -21,10 +38,18 @@ exports.loaded = function(args) {
 	page.bindingContext = pageData;
 };
 
-exports.buttonTapped = function(eventData) {
-	console.log('button tapped', eventData.eventName, eventData.object, eventData.object.text);
-	// put the emoji in the clipboard
-	clipboard.setText(eventData.object.text).then(function() {
-		console.log('OK, ' + eventData.object.text + ' copied to the clipboard');
+exports.queryChanged = function(eventData) {
+	console.log('changed', eventData, eventData.object, eventData.object.text);
+	displayableEmojiList.splice(0);
+	emojiArray.forEach(function(emoji) {
+		if (matches(emoji, eventData.object.text.toLowerCase())) {
+			displayableEmojiList.push(emoji);
+		}
 	});
-};
+	console.log('length', displayableEmojiList.length);
+}
+
+exports.itemTap = function(eventData) {
+	console.log('item tapped', eventData, eventData.object, eventData.index);
+	copy(displayableEmojiList.getItem(eventData.index).value);
+}
